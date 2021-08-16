25e4654b5a709586593e6e082c53858dfbd0b59d
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-08-16 14:54:02 +0200

    Add tests for Hero and TripSummary

---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  const expectedTags = ['tag-1', 'tag-2', 'tag-3'];
+  test('should render without crashing', () => {
+    const component = shallow(<TripSummary tags={expectedTags} />);
+    expect(component).toBeTruthy();
+  });
+
+  it('Should generate a valid link address', () => {
+    const expectedId = 'abc';
+    const generatedLink = `/trip/${expectedId}`;
+    const component = shallow(<TripSummary tags={expectedTags} id={expectedId} />);
+
+    expect(component.find('.link').prop('to')).toEqual(generatedLink);
+  });
+
+  it('should render correct image attributes', () => {
+    const expectedName = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<TripSummary image={expectedImage} name={expectedName} tags={expectedTags} />);
+
+    const renderedImageSrc = component.find('img').prop('src');
+    const renderedImageAlt = component.find('img').prop('alt');
+
+    expect(renderedImageSrc).toEqual(expectedImage);
+    expect(renderedImageAlt).toEqual(expectedName);
+  });
+
+  it('should render correct props',  () => {
+    const expectedName = 'Lorem, ipsum dolor.';
+    const costProp = '51,732.87';
+    const expectedCost = `from ${costProp}`;
+    const daysProp = 7;
+    const expectedDays = `${daysProp} days`;
+    const component = shallow(<TripSummary days={daysProp} name={expectedName} cost={costProp} tags={expectedTags} />);
+
+    const renderedName = component.find('.title').text();
+    const renderedDays = component.find('.details > span:first-child').text();
+    const renderedCost = component.find('.details > span + span').text();
+
+    expect(renderedName).toEqual(expectedName);
+    expect(renderedDays).toEqual(expectedDays);
+    expect(renderedCost).toEqual(expectedCost);
+  });
+
+  it('should throw an error if any of the props is empty', () => {
+    // {id, image, name, cost, days, tags}
+    const idProp = 7;
+    const imageProp = 'image.jpg';
+    const nameProp = 'Lorem';
+    const costProp = '54,678.21';
+    const daysProp = 4;
+
+    // const component = shallow(<TripSummary id={idProp} image={imageProp} days={daysProp} name={nameProp} cost={costProp} tags={expectedTags} />);
+
+    expect(idProp && imageProp && nameProp && costProp && daysProp).not.toBeUndefined();
+    expect(idProp && imageProp && nameProp && costProp && daysProp).not.toBeNull();
+  });
+
+  it('should render the tags in the correct order', () => {
+    const component = shallow(<TripSummary tags={expectedTags} />);
+
+    for(let i = 0; i < expectedTags.length; i++) {
+      const renderedTag = component.find('.tags > span').at(i).text();
+      expect(renderedTag).toEqual(expectedTags[i]);
+    }
+  });
+
+  it('should not render div with class tags if the property tags is a empty array or is false', () => {
+    const tagsProp = [];
+    const component = shallow(<TripSummary tags={tagsProp} />);
+    console.log(component.debug());
+  });
+});
--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -3,8 +3,32 @@ import {shallow} from 'enzyme';
 import Hero from './Hero';
 
 describe('Component Hero', () => {
-  it('should render without crashing', () => {
-    const component = shallow(<Hero titleText='Lorem ipsum' />);
+  test('should render without crashing', () => {
+    const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='image.jpg' />);
+    // console.log(component.debug());
     expect(component).toBeTruthy();
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+
+  it('should render correct title and image', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
+
+    const renderedTitle = component.find('.title').text();
+    // console.log('renderedTitle: ', renderedTitle);
+    expect(renderedTitle).toEqual(expectedTitle);
+    expect(component.find('.image').prop('src')).toEqual(expectedImage);
+  });
+
+  it('renders correct classNames', () => {
+    const mockVariants = 'small dummy';
+    const component = shallow(<Hero titleText='Lorem' imageSrc='image.jpg' variant={mockVariants} />);
+    expect(component.hasClass('component')).toBe(true);
+    expect(component.hasClass('small')).toBe(true);
+    expect(component.hasClass('dummy')).toBe(true);
   });
 });
